780535bb17cd3eaa9b4a2154c73f546be5d17ffd
Bogdan <bogdan.nourescu@cbn-it.ro>
2016-03-10 11:21:18 +0200

    mutat cbn-adauga-formular in proiect separat

---
+++ coloane_sablon.js
@@ -0,0 +1,55 @@
+if(window.Cbn===undefined){
+	window.Cbn = {};
+}
+if(window.Cbn.columns===undefined){
+	Cbn.columns = {};
+}
+Cbn.columns.sablon={
+	properties:{
+		columnsSablon: {
+			type: Array,
+			value: [
+				{
+					type: 'index',
+					caption: '',
+					style: {
+						color: 'gray'
+					}
+				},
+				{
+					name: 'numeSablon',
+					renderAs: 'text',
+					caption: 'Nume'
+				},
+				{
+					name: 'entitate',
+					renderAs: 'text',
+					caption: 'Kind'
+				},
+				{
+					name: 'tipGenerare',
+					renderAs: 'text',
+					caption: 'Tip'
+				},
+				{
+					name: 'multiplicareProcese',
+					renderAs: 'text',
+					caption: 'Multiplicare'
+				},
+				{
+					name: 'aplicare',
+					renderAs: 'text',
+					caption: 'Aplica'
+				},
+				{
+					name: 'buttons',
+					type: 'inert',
+					renderAs: 'template',
+					headStyle: "width: 100px",
+					caption: 'Buttons',
+					template: '#buttons'
+				}
+			]
+		}
+	}	
+};
